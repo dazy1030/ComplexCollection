764b4b0e24938b4e75d14ff2202fae9697b69f52
dazy1030 <odajima_naoki@cyberagent.co.jp>
2024-02-18 19:12:24 +0900

    Initial Commit

---
+++ ComplexCollection/ComplexCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexCollectionApp.swift
+//  ComplexCollection
+//
+//  Created by 小田島 直樹 on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ComplexCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
